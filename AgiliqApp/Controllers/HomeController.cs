d0ff7fa54a742881e8a6051179dc44c7b44a2538
Koundinya Goparaju <koundinya.goparaju@gmail.com>
2014-04-09 08:38:33 +0530

    Agiliq Application App

---
+++ AgiliqApp/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgiliqApp.Models;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace AgiliqApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCode()
+        {
+            GetCodeModel gcm = new GetCodeModel();
+            gcm.client_id = "KZMA1XOCwrhxF70VDMFTJ25gg1QPsJPHNunZL7vsoHHKQAT1Gj";
+            gcm.redirect_uri = "http://54.84.112.141/Home/GetAuthToken";
+            gcm.state = "12345678";
+            return View(gcm);
+            //return View();
+        }
+        public ActionResult GetAuthToken(string state="",string code="")
+        {          
+                GetCodeModel gcm = new GetCodeModel();
+                gcm.client_id = "KZMA1XOCwrhxF70VDMFTJ25gg1QPsJPHNunZL7vsoHHKQAT1Gj";
+                gcm.client_secret = "Um0Pg8upfWaA4GQPWOJ1KvzT5jctbD3U1EpAY1Y0H8KFZBjS1Z";
+                gcm.redirect_uri = "http://54.84.112.141/Home/GetAuthToken";
+                gcm.state = state;
+                gcm.code = code;
+                string requestParams = "client_id="+gcm.client_id+"&client_secret="+gcm.client_secret+"&code="+gcm.code+"&redirect_uri="+gcm.redirect_uri+"&state="+gcm.state;                
+                string requestUri = "http://join.agiliq.com/oauth/access_token/";
+                Dictionary<string, string> response = GetAsync(requestUri, requestParams);
+                gcm.access_token = response["access_token"];
+                return View("Apply", gcm);
+               // return View(gcm);
+            
+        }
+        public ActionResult Apply(string access_token, string scope, string token_type)
+        {
+            GetCodeModel gcm = new GetCodeModel();
+            gcm.access_token = access_token;
+            return View(gcm);
+
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+
+public Dictionary<string,string> GetAsync(string uri, string requestParams)
+{
+    WebClient wc = new WebClient();
+   // wc.Headers[HttpRequestHeader.ContentType] = "multipart/form-data";
+    //wc.UploadString()
+        string HtmlResult = wc.UploadString(uri,requestParams);
+    
+   FileStream fs = new FileStream("c:\\inetpub\\wwwroot\\log.txt", FileMode.Create);
+  
+    try
+    {
+        fs.Write(System.Text.Encoding.UTF8.GetBytes(HtmlResult), 0, System.Text.Encoding.UTF8.GetByteCount(HtmlResult));
+        return (Dictionary<string, string>)JsonConvert.DeserializeObject(HtmlResult, typeof(Dictionary<string, string>));
+    }
+    catch(Exception ee)
+    {
+        
+        return new Dictionary<string, string>();
+    }
+    
+}
+
+    }
+}
---
+++ AgiliqApp/Models/GetCodeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace AgiliqApp.Models
+{
+    public class GetCodeModel 
+    {
+        public string code { get; set; }
+        public string state { get; set; }
+        public string client_id { get; set; }
+        public string client_secret { get; set; }
+        public string redirect_uri { get; set; }
+        public string access_token { get; set; }
+
+
+
+    }
+}
